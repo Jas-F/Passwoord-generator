c1fcaff7e55db7dc5682edea026b79df14fbbf99
Jas-F <jasminer.franklin@gmail.com>
2020-12-17 08:48:24 -0500

    add uppercase array

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "Q", "Y", "Z"]
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
