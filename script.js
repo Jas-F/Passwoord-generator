23452a752f09a3d2dfdacf6caf6cb7f724316676
Jas-F <jasminer.franklin@gmail.com>
2020-12-18 12:43:22 -0500

    fix code

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn. = document.querySelector("#generate");
 generateBtn.addEventListener("click", writePassword);
 
-function generatePassword () {
+function generatePassword() {
 
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "q", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numericSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacterSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "="];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "q", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var numericSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var specialCharacterSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "="];
 
+  var password = [];
+  var randomIndex = [];
+
+questions()
 function questions () {
-var userInput = parseInt( prompt ("Desired Passwoord Length Between 8 - 128."));
+var userInput = parseInt( prompt ("Desired Password Length Between 8 - 128."));
+
 if (userInput > 128 || userInput < 8) {
   alert ("Must Be Between 8 - 128.")
   return
@@ -21,33 +26,25 @@ if (isNaN(userInput) === true) {
   return
 }
 
-  var lowerCaseConfirm = confirm ("Would You Like Lower Case Letters?");
-  var upperCaseConfirm = confirm ("Would You Like Upper Case Letters?");
-  var numericConfirm = confirm ("Would You Like Numeric Characters?");
-  var specialCharactersConfirm = confirm ("Would You Like Special Characters?");
+var lowerCaseConfirm = confirm ("Would You Like Lower Case Letters?");
+var upperCaseConfirm = confirm ("Would You Like Upper Case Letters?");
+var numericConfirm = confirm ("Would You Like Numeric Characters?");
+var specialCharactersConfirm = confirm ("Would You Like Special Characters?");
 
 if (lowerCaseConfirm === false && upperCaseConfirm === false && numericConfirm === false && specialCharactersConfirm === false) {
   alert ("Must Select One")
   return
   }
 
-  var options = {
-    length: userInput,
-    lowerCaseConfirm: lowerCaseConfirm,
-    upperCaseConfirm:   upperCaseConfirm,
-    numericConfirm: numericConfirm,
-    specialCharactersConfirm: specialCharactersConfirm
-  }
-  return options
-
+var questions = {
+  Length: userInput
+  upperCase: upperCaseConfirm
+  lowerCase: lowerCaseConfirm
+  number: numericConfirm
+  specialCharacter: specialCharactersConfirm
 }
 
-function randomElement(array) {
-  var randomIndex = Math.floor(Math.random()*array.length);
-  var randomElement = array[randomIndex]
-  return randomElement
-}
-
+return questions;
 }
 
 // Write password to the #password input
@@ -56,6 +53,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+}
 
 }
 
