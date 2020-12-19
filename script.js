d4e3b2d6f1508cff7c7eb9eef20d853a0140ca5b
Jas-F <jasminer.franklin@gmail.com>
2020-12-18 23:31:00 -0500

    update code

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function questions(){
 
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "q", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numericSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacterSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "="];
 
-questions()
 function questions () {
 var userInput = parseInt( prompt ("Desired Password Length Between 8 - 128."));
 
@@ -43,23 +42,26 @@ return questions;
 
 }
 
+questions()
+var randomIndex = "";
 function generatePassword() {
-for(var i=0; i < userInput; i++)  {
-  var randomIndex = Math.floor(Math.random()*Array.length);
-  password += Array[randomIndex];
+for(var i=0; i < questions; i++)  {
+randomIndex = question[Math.floor(Math.random()*Array.length)];
+password += Array[randomIndex];
+}
+return randomIndex
 }
 
-}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = randomIndex;
 
 }
-
-
+}
+);
 // Add event listener to generate button
 
