5d770f483b28e753ba2aab49921fc4ff9658ab58
Jas-F <jasminer.franklin@gmail.com>
2020-12-22 10:57:17 -0500

    fix generatepass function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", function questions(){
+generateBtn.addEventListener("click", writePassword)
 
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "q", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -31,37 +31,69 @@ if (lowerCaseConfirm === false && upperCaseConfirm === false && numericConfirm =
 }
 
 var questions = {
-  userInput: userInput,
-  upperCase: upperCaseConfirm,
-  lowerCase: lowerCaseConfirm,
+  length: userInput,
+  upperCase1: upperCaseConfirm,
+  lowerCase1: lowerCaseConfirm,
   number: numericConfirm,
   specialCharacter: specialCharactersConfirm
 }
 
 return questions;
-
 }
 
-questions()
-var randomIndex = "";
 function generatePassword() {
-for(var i=0; i < questions; i++)  {
-randomIndex = question[Math.floor(Math.random()*Array.length)];
-password += Array[randomIndex];
-}
-return randomIndex
-}
+  var passOptions = questions()
+  var finalResult = []
+  var possChar = []
+  var guaranteeChar = []
 
+  if (passOptions.specialCharacter === true){
+    possChar = possChar.concat(specialCharacterSet)
+    guaranteeChar.push(random(specialCharacterSet))
+  }
+  if (passOptions.upperCase1 === true){
+    possChar = possChar.concat(upperCase)
+    guaranteeChar.push(random(upperCase))
+  }
+  if (passOptions.lowerCase1 === true){
+    possChar = possChar.concat(lowerCase)
+    guaranteeChar.push(random(lowerCase))
+  }
+  if (passOptions.number === true){
+    possChar = possChar.concat(numericSet)
+    guaranteeChar.push(random(numericSet))
+  }
+  console.log(possChar)
+  console.log(guaranteeChar)
+  for(var i=0; i < passOptions.length; i++) {
+    var finalChar = random(possChar)
+    finalResult.push(finalChar)
+  }
+  for(var i=0; i < guaranteeChar.length; i++) {
+    finalResult[i]=guaranteeChar[i]
+  }
+  return finalResult.join("")
+}
+// for(var i=0; i < questions; i++)  {
+// random = question[Math.floor(Math.random()*Array.length)];
+// password += Array[randomIndex];
+// }
+// console.log(randomIndex)
+// return randomIndex
+// }
+function random(array){
+  var randomIndex = Math.floor(Math.random()*array.length)
+  var randomEl = array[randomIndex]
+    return randomEl
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = randomIndex;
+  passwordText.value = password;
 
 }
-}
-);
 // Add event listener to generate button
 
